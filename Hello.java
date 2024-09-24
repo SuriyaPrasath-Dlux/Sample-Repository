a3dec40295d60ef213b5b6c6a62d1c6e8d1cd64a
Suriya prasath K <suriyaprasath.k@dluxtech.com>
2024-09-24 14:30:43 +0530

    updated code

--- Hello.java
+++ Hello.java
@@ -3,7 +3,9 @@ import java.util.Scanner;
 public class Hello {
     public static void main(String[] args) {
         Scanner input =new Scanner(System.in);
+        System.out.println("enter a");
         int a=input.nextInt();
+        System.out.println("enter b");
         int b= input.nextInt();
         int c=a+b;
         System.out.println(c);
